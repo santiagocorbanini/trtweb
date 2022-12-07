b87b381ea175f088b88dc7ddbc27992b825258f6
Franco Vanney <francoavanney@gmail.com>
2022-12-07 17:59:22 -0300

    navbar e inicio

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -8,213 +8,185 @@ const Nav = () => {
     const { isTabletOrMobile } = MediaQueries()
     return (
         <nav className="navbar navbar-expand-lg fixed-top navbarcustom">
-            <div className="container">
-                <button
-                    className="navbar-toggler text-uppercase font-weight-bold text-white"
-                    id="menu-text"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarResponsive"
-                    aria-controls="navbarResponsive"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    =
-                </button>
-                <img
-                    src={logo}
-                    alt={logo}
-                    id="logo-responsive"
-                    width={90}
-                    height={15}
-                    onClick={() => {
-                        navigate("/nosotros")
-                    }}
-                />
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav">
+            <button
+                className="navbar-toggler text-uppercase font-weight-bold text-white"
+                id="menu-text"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                =
+            </button>
+{/*             <img
+                src={logo}
+                alt={logo}
+                id="logo-responsive"
+                width={90}
+                height={15}
+                onClick={() => {
+                    navigate("/nosotros")
+                }}
+            />
+            <img
+                src={logo}
+                alt={logo}
+                id="logo"
+                width={132}
+                height={23}
+                onClick={() => {
+                    navigate("/nosotros")
+                }}
+            /> */}
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav">
                     <li className="nav-link mx-0 mx-lg-1">
-                            {isTabletOrMobile ? (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/inicio")
-                                    }}
-                                    data-bs-toggle="collapse"
-                                    data-bs-target="#navbarResponsive"
-                                >
-                                    Inicio
-                                </button>
-                            ) : (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/inicio")
-                                    }}
-                                >
-                                    Inicio
-                                </button>
-                            )}
-                        </li>
+                        {isTabletOrMobile ? (
+                            <button
+                                className="nav-link"
+                                onClick={() => {
+                                    navigate("/inicio")
+                                }}
+                                data-bs-toggle="collapse"
+                                data-bs-target="#navbarResponsive"
+                            >
+                                Inicio
+                            </button>
+                        ) : (
+                            <button
+                                className="nav-link"
+                                onClick={() => {
+                                    navigate("/inicio")
+                                }}
+                            >
+                                Inicio
+                            </button>
+                        )}
+                    </li>
 
-                        <li className="nav-link mx-0 mx-lg-1">
-                            {isTabletOrMobile ? (
-                                <>
-                                    <button
-                                        className="nav-link"
-                                        onClick={() => {
-                                            navigate("/nosotros")
-                                        }}
-                                        data-bs-toggle="collapse"
-                                        data-bs-target="#navbarResponsive"
-                                    >
-                                        Nosotros
-                                    </button>
-                                </>
-                            ) : (
+                    <li className="nav-link mx-0 mx-lg-1">
+                        {isTabletOrMobile ? (
+                            <>
                                 <button
                                     className="nav-link"
                                     onClick={() => {
                                         navigate("/nosotros")
                                     }}
-                                >
-                                    Nosotros
-                                </button>
-                            )}
-                        </li>
-                
-                        <li className="nav-link mx-0 mx-lg-1">
-                            {isTabletOrMobile ? (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/fechas")
-                                    }}
                                     data-bs-toggle="collapse"
                                     data-bs-target="#navbarResponsive"
                                 >
-                                    Fechas
+                                    Festival
                                 </button>
-                            ) : (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/fechas")
-                                    }}
-                                >
-                                    Fechas
-                                </button>
-                            )}
-                        </li>
-                        <div className="logo-container">
-                            <img
-                                src={logo}
-                                alt={logo}
-                                id="logo"
-                                width={132}
-                                height={23}
+                            </>
+                        ) : (
+                            <button
+                                className="nav-link"
                                 onClick={() => {
                                     navigate("/nosotros")
                                 }}
-                            />
-                        </div>
-                        {/*
-                        <li className="nav-link mx-0 mx-lg-1">
-                            {isTabletOrMobile ? (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/trabajos")
-                                    }}
-                                    data-bs-toggle="collapse"
-                                    data-bs-target="#navbarResponsive"
-                                >
-                                    Trabajos
-                                </button>
-                            ) : (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/trabajos")
-                                    }}
-                                >
-                                    Trabajos
-                                </button>
-                            )}
-                        </li>
-                        */}
-                        <li className="nav-link mx-0 mx-lg-1">
-                            {isTabletOrMobile ? (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/eventos")
-                                    }}
-                                    data-bs-toggle="collapse"
-                                    data-bs-target="#navbarResponsive"
-                                >
-                                    Eventos
-                                </button>
-                            ) : (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/eventos")
-                                    }}
-                                >
-                                    Eventos
-                                </button>
-                            )}
-                        </li>
-                        <li className="nav-link mx-0 mx-lg-1">
-                            {isTabletOrMobile ? (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/festival")
-                                    }}
-                                    data-bs-toggle="collapse"
-                                    data-bs-target="#navbarResponsive"
-                                >
-                                    Festival
-                                </button>
-                            ) : (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/festival")
-                                    }}
-                                >
-                                    Festival
-                                </button>
-                            )}
-                        </li>
-                        <li className="nav-link mx-0 mx-lg-1">
-                            {isTabletOrMobile ? (
-                                <button
-                                    className="nav-link"
-                                    onClick={() => {
-                                        navigate("/contacto")
-                                    }}
-                                    data-bs-toggle="collapse"
-                                    data-bs-target="#navbarResponsive"
-                                >
-                                    Contacto
-                                </button>
-                            ) : (
-                                <button
-                                    className="nav-link"
-                                    id="contact-buton"
-                                    onClick={() => {
-                                        navigate("/contacto")
-                                    }}
-                                >
-                                    Contacto
-                                </button>
-                            )}
-                        </li>
-                    </ul>
-                </div>
+                            >
+                                Festival
+                            </button>
+                        )}
+                    </li>
+
+                    <li className="nav-link mx-0 mx-lg-1">
+                        {isTabletOrMobile ? (
+                            <button
+                                className="nav-link"
+                                onClick={() => {
+                                    navigate("/fechas")
+                                }}
+                                data-bs-toggle="collapse"
+                                data-bs-target="#navbarResponsive"
+                            >
+                                Eventos Realizados
+                            </button>
+                        ) : (
+                            <button
+                                className="nav-link"
+                                onClick={() => {
+                                    navigate("/fechas")
+                                }}
+                            >
+                                Eventos Realizados
+                            </button>
+                        )}
+                    </li>
+
+                    <li className="nav-link mx-0 mx-lg-1">
+                        {isTabletOrMobile ? (
+                            <button
+                                className="nav-link"
+                                onClick={() => {
+                                    navigate("/eventos")
+                                }}
+                                data-bs-toggle="collapse"
+                                data-bs-target="#navbarResponsive"
+                            >
+                                Proximas Fechas
+                            </button>
+                        ) : (
+                            <button
+                                className="nav-link"
+                                onClick={() => {
+                                    navigate("/eventos")
+                                }}
+                            >
+                                Proximas Fechas
+                            </button>
+                        )}
+                    </li>
+                    <li className="nav-link mx-0 mx-lg-1">
+                        {isTabletOrMobile ? (
+                            <button
+                                className="nav-link"
+                                onClick={() => {
+                                    navigate("/festival")
+                                }}
+                                data-bs-toggle="collapse"
+                                data-bs-target="#navbarResponsive"
+                            >
+                                Nosotros
+                            </button>
+                        ) : (
+                            <button
+                                className="nav-link"
+                                onClick={() => {
+                                    navigate("/festival")
+                                }}
+                            >
+                                Nosotros
+                            </button>
+                        )}
+                    </li>
+                    <li className="nav-link mx-0 mx-lg-1">
+                        {isTabletOrMobile ? (
+                            <button
+                                className="nav-link"
+                                onClick={() => {
+                                    navigate("/contacto")
+                                }}
+                                data-bs-toggle="collapse"
+                                data-bs-target="#navbarResponsive"
+                            >
+                                Contacto
+                            </button>
+                        ) : (
+                            <button
+                                className="nav-link"
+                                id="contact-buton"
+                                onClick={() => {
+                                    navigate("/contacto")
+                                }}
+                            >
+                                Contacto
+                            </button>
+                        )}
+                    </li>
+                </ul>
             </div>
         </nav>
     )
