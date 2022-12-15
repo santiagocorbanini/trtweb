907ac47406eb32942bbbe529a860a90c81a4f35d
Santiago Corbanini <santiagocorbanini@gmail.com>
2022-12-14 22:47:05 -0300

    Fechas - acomodo divs

--- src/components/Fechas/index.jsx
+++ src/components/Fechas/index.jsx
@@ -37,33 +37,37 @@ const Fechas = () => {
                     <img src={ciro} alt='ciro' className="w-100"/>
                 </div>
                 <div className="col-sm" id="blanco">
-                    <h1 className="m-2 title-artist">CIRO Y LOS PERSAS</h1>
-                    <h4 className="m-2 title-artist">4 de Octubre / Barcelona Tour</h4>
-                    <a href="https://www.instagram.com/2222agencia/" target={"_blank"} rel={"noreferrer"}>
-                    <img
-                        src={verEventoNegro}
-                        width={60}
-                        height={60}
-                        alt={verEventoNegro}
-                        />
-                    </a>
+                    <div className="col-sm" id="div-fechas">
+                        <h1 className="m-2 title-fecha">CIRO Y LOS PERSAS</h1>
+                        <h4 className="m-2 date-fecha">4 de Octubre / Barcelona Tour</h4>
+                        <a href="https://www.instagram.com/2222agencia/" target={"_blank"} rel={"noreferrer"}>
+                        <img
+                            src={verEventoNegro}
+                            width={60}
+                            height={60}
+                            alt={verEventoNegro}
+                            />
+                        </a>
+                    </div>
                 </div>
             </div>
             <div className="row g-0">
                 <div className="col-sm">
                     <img src={cris} alt='ciro' className="w-100"/>
                 </div>
-                <div className="col-sm">
-                    <h1 className="m-2 title-artist">CRUZANDO EL CHARCO</h1>
-                    <h4 className="m-2 title-artist">4 de Octubre / Barcelona Tour</h4>
-                    <a href="https://www.instagram.com/2222agencia/" target={"_blank"} rel={"noreferrer"}>
-                    <img
-                        src={verEventoBlanco}
-                        width={60}
-                        height={60}
-                        alt={verEventoBlanco}
-                        />
-                    </a>
+                <div className="col-sm">  
+                    <div className="col-sm" id="div-fechas">
+                        <h1 className="m-2 title-fecha">CRUZANDO EL CHARCO</h1>
+                        <h4 className="m-2 date-fecha">4 de Octubre / Barcelona Tour</h4>
+                        <a href="https://www.instagram.com/2222agencia/" target={"_blank"} rel={"noreferrer"}>
+                        <img
+                            src={verEventoBlanco}
+                            width={60}
+                            height={60}
+                            alt={verEventoBlanco}
+                            />
+                        </a>
+                    </div>
                 </div>
             </div>
 
